fb27575832df8fa1e507c021708acef94b390cc0
krisnapradeep <krisnapradeep@yahoo.com>
2025-02-04 19:20:05 +0530

    4th Feb 2025 env config fix

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,12 +1,9 @@
 // src/config/config.ts
-
 import axios from 'axios';
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const AUTH_TOKEN_KEY = import.meta.env.VITE_AUTH_TOKEN_KEY;
-
-axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem(AUTH_TOKEN_KEY)}`;
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}` 
 
 export const axiosInstance = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: API_BASE_URL, 
 });
