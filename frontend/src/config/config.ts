3a57dd183646e4af2134c761005df8fc6f1fcef1
krisnapradeep <krisnapradeep@yahoo.com>
2025-02-04 15:03:29 +0530

    4rd feb 2025 config

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -2,9 +2,11 @@
 
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://localhost:5005/api';
-axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}` 
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const AUTH_TOKEN_KEY = import.meta.env.VITE_AUTH_TOKEN_KEY;
+
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem(AUTH_TOKEN_KEY)}`;
 
 export const axiosInstance = axios.create({
-    baseURL: API_BASE_URL, 
+    baseURL: API_BASE_URL,
 });
